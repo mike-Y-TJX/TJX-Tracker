347323b2d497cb786b03517e06630fb70a22efcb
Matthew Iaconis <mat03131@tjx.com>
2021-11-15 09:40:41 -0500

    started mattDev

--- Application/routes/customers.js
+++ Application/routes/customers.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var customersMockData = require("../mockdata/customers")
+var customersMockData = require('../mockdata/customers');
 
+router.route('/').get((req, res, next) => {
+	res.json(customersMockData);
+});
 
-router
-.route('/')
-.get((req, res, next) => {
-    res.json(customersMockData)
-    
-})
+router.route('/:id').get((req, res, next) => {
+	res.send(`hello number ${req.params.id}`);
+});
 
 module.exports = router;
