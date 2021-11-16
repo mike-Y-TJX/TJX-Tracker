8c625324eabcf619790ac3d1130516b68199200f
ievafil <92386141+ievafil@users.noreply.github.com>
2021-11-16 17:03:05 +0000

    products.js working and tested

--- Application/routes/products.js
+++ Application/routes/products.js
@@ -3,81 +3,82 @@ var router = express.Router();
 const db = require('../config/database/db');
 
 router.route('/').get(async (req, res, next) => {
-	try {
-		//SQL query
-		const [rows, fields] = await db.query(
-			`SELECT * FROM products LIMIT 1000;`
-		);
+    try {
+        //SQL query
+        const [rows, fields] = await db.query(
+            `SELECT * FROM products LIMIT 1000;`
+        );
 
-		// validate db returned resutls, return to user or throw error
-		if (rows && rows.length > 0) {
-			res.json(rows);
-		} else {
-			throw new Error('No products found');
-		}
-	} catch (er) {
-		res.status(400).send('No products found');
-	}
+        // validate db returned resutls, return to user or throw error
+        if (rows && rows.length > 0) {
+            res.json(rows);
+        } else {
+            throw new Error('No products found');
+        }
+    } catch (er) {
+        res.status(400).send('No products found');
+    }
 });
 
 router
-	.route('/:id')
-	.get(async (req, res, next) => {
-		try {
-			// SQL query
-			const [rows, fields] = await db.query(
-				`SELECT * FROM products
+    .route('/:id')
+    .get(async (req, res, next) => {
+        try {
+            // SQL query
+            const [rows, fields] = await db.query(
+                `SELECT * FROM products
                 WHERE product_id = ?;`,
-				[req.params.id]
-			);
+                [req.params.id]
+            );
 
-			// validate db returned results, return to user or throw error
-			if (rows && rows.length > 0) {
-				res.json(rows);
-			} else {
-				throw new Error('No product found');
-			}
-		} catch (er) {
-			res.status(400).send('No product found');
-		}
-	})
-	.put(async (req, res, next) => {
-		const updatedProduct = req.body;
+            // validate db returned results, return to user or throw error
+            if (rows && rows.length > 0) {
+                res.json(rows);
+            } else {
+                throw new Error('No product found');
+            }
+        } catch (er) {
+            res.status(400).send('No product found');
+        }
+    })
+    .put(async (req, res, next) => {
+        const updatedProduct = req.body;
 
-		try {
-			await db.beginTransaction();
-
-			const existingProductUpdated = await db.query(
-				`UPDATE product SET
-                product_SKU = ?,
-				product_price = ?,
+        try {
+            await db.beginTransaction();
+            console.log(updatedProduct);
+            const existingProductUpdated = await db.query(
+                `UPDATE Products SET
+                product_sku = ?,
+                product_price = ?,
                 product_name = ?,
                 product_quantity = ?,
                 product_description = ?,
                 image_url = ?
                 WHERE product_id = ?;`,
-				[
-					updatedProduct.product_SKU,
-					updatedProduct.product_price,
-					updatedProduct.product_name,
-					updatedProduct.product_quantity,
-					updatedProduct.product_description,
-					updatedProduct.image_url,
-					req.params.id,
-				]
-			);
+                [
+                    updatedProduct.product_sku,
+                    updatedProduct.product_price,
+                    updatedProduct.product_name,
+                    updatedProduct.product_quantity,
+                    updatedProduct.product_description,
+                    updatedProduct.image_url,
+                    req.params.id,
+                ]
+            );
+            console.log(existingProductUpdated);
+            await db.commit();
 
-			await db.commit();
-
-			// validate database was updated
-			if (existingProductUpdated[0].affectedRows > 0) {
-				res.json(updatedProduct);
-			} else {
-				throw new Error('Product not updated');
-			}
-		} catch (er) {
-			res.status(400).send('Product not updated');
-		}
-	});
+            // validate database was updated
+            //  console.log(existingProductUpdated);
+            if (existingProductUpdated[0].affectedRows > 0) {
+                res.json(updatedProduct);
+            } else {
+                throw new Error('Product not updated 1');
+            }
+        } catch (er) {
+            res.status(400).send('Product not updated 2');
+        }
+    });
 
 module.exports = router;
