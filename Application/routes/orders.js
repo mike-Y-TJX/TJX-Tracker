0743d83816d65331884586324a09efc3ffb0fbe5
Michael Yohannes <mic12685@tjx.com>
2021-11-17 08:06:47 -0500

    GET /order/:id implemented

--- Application/routes/orders.js
+++ Application/routes/orders.js
@@ -32,6 +32,9 @@ router
 			p.product_id = od.product_id
 			;`,
 			(error, results, fields) => {
+				if(error || results.length == 0){
+					return res.status(400).send("No Orders")
+				}
 				let orderData = []
 				let processedOrders = {}
 
@@ -129,65 +132,70 @@ router
 			;`,
 			[req.params.id],
 			(error, results, fields) => {
-				var resultsArray = results.map((mysqlObj, index) => {
+				if(error || results.length == 0){
+					return res.status(400).send("Order Doesnt Exist")
+				}
+				var results = results.map((mysqlObj, index) => {
 					return Object.assign({}, mysqlObj);
 				});
-				console.log(resultsArray)
-				// let orderData = []
-				// let processedOrders = {}
+				
+				let orderData = []
+				let processedOrders = {}
 
-				// results.forEach(order => {
-				// 	if (!processedOrders[order.order_id]){
-				// 		processedOrders[order.order_id] = [order.detail_id];
-				// 		orderData.push({
-				// 			order_id: order.order_id,
-				// 			order_notes: order.order_notes,
-				// 			datetime_order_placed: order.datetime_order_placed,
-				// 			status_desc: order.status_desc,
-				// 			customer_detail: {
-				// 				first_name: order.first_name,
-				// 				middle_name: order.middle_name,
-				// 				last_name: order.last_name,
-				// 				phone_country_code: order.phone_country_code,
-				// 				phone: order.phone,
-				// 				email: order.email,
-				// 				customer_notes: order.customer_notes,
-				// 				street: order.street,
-				// 				city: order.city,
-				// 				zip_code: order.zip_code,
-				// 				country: order.country,
-				// 			},
-				// 			order_detail:[
-				// 				{
-				// 					quantity_purchased: order.quantity_purchased,
-				// 					product_id: order.product_id, 
-				// 					product_sku: order.product_sku, 
-				// 					product_price: order.product_price, 
-				// 					product_name: order.product_name, 
-				// 					product_quantity: order.product_quantity, 
-				// 					product_description: order.product_description, 
-				// 					image_url: order.image_url,
-				// 				}
-				// 			]
+				results.forEach(order => {
+					if (!processedOrders[order.order_id]){
+						processedOrders[order.order_id] = [order.detail_id];
+						orderData.push({
+							order_id: order.order_id,
+							order_notes: order.order_notes,
+							datetime_order_placed: order.datetime_order_placed,
+							status_desc: order.status_desc,
+							customer_detail: {
+								first_name: order.first_name,
+								middle_name: order.middle_name,
+								last_name: order.last_name,
+								phone_country_code: order.phone_country_code,
+								phone: order.phone,
+								email: order.email,
+								customer_notes: order.customer_notes,
+								street: order.street,
+								city: order.city,
+								zip_code: order.zip_code,
+								country: order.country,
+							},
+							order_detail:[
+								{
+									quantity_purchased: order.quantity_purchased,
+									product_id: order.product_id, 
+									product_sku: order.product_sku, 
+									product_price: order.product_price, 
+									product_name: order.product_name, 
+									product_quantity: order.product_quantity, 
+									product_description: order.product_description, 
+									image_url: order.image_url,
+								}
+							]
 
-				// 		})
-				// 	} else {
-				// 		let orderIndex = orderData.findIndex((orders) => {
-				// 			return orders.order_id === order.order_id
-				// 		})
-				// 		orderData[orderIndex].order_detail.push(
-				// 			{
-				// 				quantity_purchased: order.quantity_purchased,
-				// 				product_id: order.product_id, 
-				// 				product_sku: order.product_sku, 
-				// 				product_price: order.product_price, 
-				// 				product_name: order.product_name, 
-				// 				product_quantity: order.product_quantity, 
-				// 				product_description: order.product_description, 
-				// 				image_url: order.image_url,
-				// 			}
-				// 		)
-				// 	}
+						})
+					} else {
+						let orderIndex = orderData.findIndex((orders) => {
+							return orders.order_id === order.order_id
+						})
+						orderData[orderIndex].order_detail.push(
+							{
+								quantity_purchased: order.quantity_purchased,
+								product_id: order.product_id, 
+								product_sku: order.product_sku, 
+								product_price: order.product_price, 
+								product_name: order.product_name, 
+								product_quantity: order.product_quantity, 
+								product_description: order.product_description, 
+								image_url: order.image_url,
+							}
+						)
+					}
+				});
+				res.json(orderData)
 
 				});
 	})
