d206080a9108784695cd9e59ed731fa90a8fc6d1
Michael Yohannes <mic12685@tjx.com>
2021-11-17 15:14:51 -0500

    fullstack deploy

--- Application/app.js
+++ Application/app.js
@@ -25,7 +25,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(path.join(__dirname, '..'),"/FrontEndDev" )))
 
 app.use("/api/customers", customerRoutes)
 app.use("/api/orders", orderRoutes)
@@ -33,9 +35,9 @@ app.use("/api/products", productRoutes)
 app.use("/csr", csrRoutes)
 
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+// app.get("/", (req, res) => {
+//     res.render("index")
+// })
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
