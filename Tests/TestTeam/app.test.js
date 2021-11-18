3ca6624e0ab06dfb565660a45eb680938f20d668
Joel Ogualiri <joe00852@tjx.com>
2021-11-18 01:49:12 -0500

    completed tests for Customers endpoint

--- Tests/TestTeam/app.test.js
+++ Tests/TestTeam/app.test.js
@@ -16,12 +16,17 @@ describe('CUSTOMERS', () => {
             const response = await request.get("/api/customers")
             expect(response.statusCode).toBe(200)
         })
+        test("the response must be customers object", async () => {
 
+            const response = await request.get("/api/customers")
+            expect(typeof(response)).toBe("object")
+        })
         test("the response must have at least one customer", async () => {
 
             const response = await request.get("/api/customers")
-            expect(response.body).not.toHaveLength(200)
+            expect(response.body).not.toHaveLength(0)
         })
+
 
     })
 
@@ -91,7 +96,7 @@ describe('CUSTOMERS', () => {
             expect(response.body.customer_id).toBeDefined()
 
         })
-        test('should respond with a status code of 400 for invalid post', async () => {
+        test('should respond with a status code of 400 and appropriate message for invalid post', async () => {
 
             const bodyData = [
                 { "customer_id": 1004 },
@@ -113,6 +118,7 @@ describe('CUSTOMERS', () => {
             for (const body of bodyData) {
                 const response = await request.post("/api/customers").send(body)
                 expect(response.statusCode).toBe(400)
+                expect(response.text).toBe("Customer Not Added")
             }
 
         })
@@ -184,7 +190,58 @@ describe('CUSTOMERS', () => {
                 expect(response.text).toBe("Invalid Customer")
 
         })
+
+        test('should respond with a status code of 400 and appropriate message for invalid parameters', async () => {
+
+            const bodyData = [
+               
+                { "first_name": 12},
+                { "middle_name": 12 },
+                { "last_name": 12},
+                { "phone_country_code": "0" },
+                { "phone":  "000000"},
+                { "email": 12 },
+                { "customer_notes": 12},
+                { "street":  12},
+                { "city":  21},
+                { "zip_code": 21 },
+                { "country": 12 },
+                
+
+            ]
+
+            for (const body of bodyData) {
+                const testId = "1"
+            
+                const response = await request.put(`/api/customers/${testId}`).send(body)
+                expect(response.statusCode).toBe(400)
+                expect(response.text).toBe("Invalid Parameters")
+            }
+
+        })
         
+    })
+
+    describe('DELETE method for CUSTOMER ID endpoint', ()=>{
+
+        test("return a 200 status code for deleted customer", async () => {
+
+            const first_response = await request.get("/api/customers")
+
+            globalThis.last_customer_id = first_response.body[first_response.body.length-1]["customer_id"]
+            globalThis.last_customer_firstname_old = first_response.body[first_response.body.length-1]["first_name"]
+            
+            const response = await request.delete(`/api/customers/${last_customer_id}`)
+            expect(response.statusCode).toBe(200)
+
+            
+    
+        })
+        test("return undefined when probed for previously deleted customer", async () => {
+            const response = await request.get(`/api/customers/${last_customer_id}`)
+            expect(response.body.length).toBe(undefined)
+        })
+
     })
 
 
